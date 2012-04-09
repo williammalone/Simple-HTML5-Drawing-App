aa24292c9fb2e5bccf4c3a637a43def6ee3ead5b
William Malone <w@williammalone.com>
2012-04-09 15:28:26 -0600

    Removed jQuery and added touch event support

--- html5-canvas-drawing-app.js
+++ html5-canvas-drawing-app.js
@@ -14,7 +14,7 @@
 // limitations under the License.
 
 /*jslint browser: true */
-/*global G_vmlCanvasManager, $ */
+/*global G_vmlCanvasManager */
 
 var drawingApp = (function () {
 
@@ -70,9 +70,6 @@ var drawingApp = (function () {
 		// Clears the canvas.
 		clearCanvas = function () {
 
-			//context.fillStyle = '#ffffff'; // Work around for Chrome
-			//context.fillRect(0, 0, canvasWidth, canvasHeight); // Fill in the canvas with white
-			//canvas.width = canvas.width; // clears the canvas 
 			context.clearRect(0, 0, canvasWidth, canvasHeight);
 		},
 
@@ -82,7 +79,49 @@ var drawingApp = (function () {
 			var locX,
 				locY,
 				radius,
-				i;
+				i,
+				selected,
+
+				drawCrayon = function (x, y, color, selected) {
+
+					context.beginPath();
+					context.moveTo(x + 41, y + 11);
+					context.lineTo(x + 41, y + 35);
+					context.lineTo(x + 29, y + 35);
+					context.lineTo(x + 29, y + 33);
+					context.lineTo(x + 11, y + 27);
+					context.lineTo(x + 11, y + 19);
+					context.lineTo(x + 29, y + 13);
+					context.lineTo(x + 29, y + 11);
+					context.lineTo(x + 41, y + 11);
+					context.closePath();
+					context.fillStyle = color;
+					context.fill();
+
+					if (selected) {
+						context.drawImage(crayonImage, x, y, mediumImageWidth, mediumImageHeight);
+					} else {
+						context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, x, y, 59, mediumImageHeight);
+					}
+				},
+
+				drawMarker = function (x, y, color, selected) {
+
+					context.beginPath();
+					context.moveTo(x + 10, y + 24);
+					context.lineTo(x + 10, y + 24);
+					context.lineTo(x + 22, y + 16);
+					context.lineTo(x + 22, y + 31);
+					context.closePath();
+					context.fillStyle = color;
+					context.fill();
+
+					if (selected) {
+						context.drawImage(markerImage, x, y, mediumImageWidth, mediumImageHeight);
+					} else {
+						context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, x, y, 59, mediumImageHeight);
+					}
+				};
 
 			// Make sure required resources are loaded before redrawing
 			if (curLoadResNum < totalLoadResources) {
@@ -96,186 +135,66 @@ var drawingApp = (function () {
 				// Draw the crayon tool background
 				context.drawImage(crayonBackgroundImage, 0, 0, canvasWidth, canvasHeight);
 
-				// Purple
-				locX = (curColor === colorPurple) ? 18 : 52;
+				// Draw purple crayon
+				selected = (curColor === colorPurple);
+				locX = selected ? 18 : 52;
 				locY = 19;
+				drawCrayon(locX, locY, colorPurple, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 41, locY + 11);
-				context.lineTo(locX + 41, locY + 35);
-				context.lineTo(locX + 29, locY + 35);
-				context.lineTo(locX + 29, locY + 33);
-				context.lineTo(locX + 11, locY + 27);
-				context.lineTo(locX + 11, locY + 19);
-				context.lineTo(locX + 29, locY + 13);
-				context.lineTo(locX + 29, locY + 11);
-				context.lineTo(locX + 41, locY + 11);
-				context.closePath();
-				context.fillStyle = colorPurple;
-				context.fill();
-
-				if (curColor === colorPurple) {
-					context.drawImage(crayonImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Green
-				locX = (curColor === colorGreen) ? 18 : 52;
+				// Draw green crayon
+				selected = (curColor === colorGreen);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawCrayon(locX, locY, colorGreen, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 41, locY + 11);
-				context.lineTo(locX + 41, locY + 35);
-				context.lineTo(locX + 29, locY + 35);
-				context.lineTo(locX + 29, locY + 33);
-				context.lineTo(locX + 11, locY + 27);
-				context.lineTo(locX + 11, locY + 19);
-				context.lineTo(locX + 29, locY + 13);
-				context.lineTo(locX + 29, locY + 11);
-				context.lineTo(locX + 41, locY + 11);
-				context.closePath();
-				context.fillStyle = colorGreen;
-				context.fill();
-
-				if (curColor === colorGreen) {
-					context.drawImage(crayonImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Yellow
-				locX = (curColor === colorYellow) ? 18 : 52;
+				// Draw yellow crayon
+				selected = (curColor === colorYellow);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawCrayon(locX, locY, colorYellow, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 41, locY + 11);
-				context.lineTo(locX + 41, locY + 35);
-				context.lineTo(locX + 29, locY + 35);
-				context.lineTo(locX + 29, locY + 33);
-				context.lineTo(locX + 11, locY + 27);
-				context.lineTo(locX + 11, locY + 19);
-				context.lineTo(locX + 29, locY + 13);
-				context.lineTo(locX + 29, locY + 11);
-				context.lineTo(locX + 41, locY + 11);
-				context.closePath();
-				context.fillStyle = colorYellow;
-				context.fill();
-
-				if (curColor === colorYellow) {
-					context.drawImage(crayonImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Yellow
-				locX = (curColor === colorBrown) ? 18 : 52;
+				// Draw brown crayon
+				selected = (curColor === colorBrown);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawCrayon(locX, locY, colorBrown, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 41, locY + 11);
-				context.lineTo(locX + 41, locY + 35);
-				context.lineTo(locX + 29, locY + 35);
-				context.lineTo(locX + 29, locY + 33);
-				context.lineTo(locX + 11, locY + 27);
-				context.lineTo(locX + 11, locY + 19);
-				context.lineTo(locX + 29, locY + 13);
-				context.lineTo(locX + 29, locY + 11);
-				context.lineTo(locX + 41, locY + 11);
-				context.closePath();
-				context.fillStyle = colorBrown;
-				context.fill();
-
-				if (curColor === colorBrown) {
-					context.drawImage(crayonImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(crayonImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
 			} else if (curTool === "marker") {
 
 				// Draw the marker tool background
 				context.drawImage(markerBackgroundImage, 0, 0, canvasWidth, canvasHeight);
 
-				// Purple
-				locX = (curColor === colorPurple) ? 18 : 52;
+				// Draw purple marker
+				selected = (curColor === colorPurple);
+				locX = selected ? 18 : 52;
 				locY = 19;
+				drawMarker(locX, locY, colorPurple, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 10, locY + 24);
-				context.lineTo(locX + 10, locY + 24);
-				context.lineTo(locX + 22, locY + 16);
-				context.lineTo(locX + 22, locY + 31);
-				context.closePath();
-				context.fillStyle = colorPurple;
-				context.fill();
-
-				if (curColor === colorPurple) {
-					context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Green
-				locX = (curColor === colorGreen) ? 18 : 52;
+				// Draw green marker
+				selected = (curColor === colorGreen);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawMarker(locX, locY, colorGreen, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 10, locY + 24);
-				context.lineTo(locX + 10, locY + 24);
-				context.lineTo(locX + 22, locY + 16);
-				context.lineTo(locX + 22, locY + 31);
-				context.closePath();
-				context.fillStyle = colorGreen;
-				context.fill();
-
-				if (curColor === colorGreen) {
-					context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Yellow
-				locX = (curColor === colorYellow) ? 18 : 52;
+				// Draw yellow marker
+				selected = (curColor === colorYellow);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawMarker(locX, locY, colorYellow, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 10, locY + 24);
-				context.lineTo(locX + 10, locY + 24);
-				context.lineTo(locX + 22, locY + 16);
-				context.lineTo(locX + 22, locY + 31);
-				context.closePath();
-				context.fillStyle = colorYellow;
-				context.fill();
-
-				if (curColor === colorYellow) {
-					context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
-
-				// Yellow
-				locX = (curColor === colorBrown) ? 18 : 52;
+				// Draw brown marker
+				selected = (curColor === colorBrown);
+				locX = selected ? 18 : 52;
 				locY += 46;
+				drawMarker(locX, locY, colorBrown, selected);
 
-				context.beginPath();
-				context.moveTo(locX + 10, locY + 24);
-				context.lineTo(locX + 10, locY + 24);
-				context.lineTo(locX + 22, locY + 16);
-				context.lineTo(locX + 22, locY + 31);
-				context.closePath();
-				context.fillStyle = colorBrown;
-				context.fill();
-
-				if (curColor === colorBrown) {
-					context.drawImage(markerImage, locX, locY, mediumImageWidth, mediumImageHeight);
-				} else {
-					context.drawImage(markerImage, 0, 0, 59, mediumImageHeight, locX, locY, 59, mediumImageHeight);
-				}
 			} else if (curTool === "eraser") {
+
 				context.drawImage(eraserBackgroundImage, 0, 0, canvasWidth, canvasHeight);
 				context.drawImage(eraserImage, 18, 19, mediumImageWidth, mediumImageHeight);
 			}
 
+			// Draw line on ruler to indicate size
 			switch (curSize) {
 			case "small":
 				locX = 467;
@@ -305,8 +224,10 @@ var drawingApp = (function () {
 			context.rect(drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
 			context.clip();
 
+			// For each point drawn
 			for (i = 0; i < clickX.length; i += 1) {
 
+				// Set the drawing radius
 				switch (clickSize[i]) {
 				case "small":
 					radius = 2;
@@ -324,7 +245,9 @@ var drawingApp = (function () {
 					break;
 				}
 
+				// Set the drawing path
 				context.beginPath();
+				// If dragging then draw a line between the two points
 				if (clickDrag[i] && i) {
 					context.moveTo(clickX[i - 1], clickY[i - 1]);
 				} else {
@@ -333,6 +256,7 @@ var drawingApp = (function () {
 				context.lineTo(clickX[i], clickY[i]);
 				context.closePath();
 
+				// Set the drawing color
 				if (clickTool[i] === "eraser") {
 					//context.globalCompositeOperation = "destination-out"; // To erase instead of draw over with white
 					context.strokeStyle = 'white';
@@ -358,15 +282,6 @@ var drawingApp = (function () {
 			context.drawImage(outlineImage, drawingAreaX, drawingAreaY, drawingAreaWidth, drawingAreaHeight);
 		},
 
-		// Calls the redraw function after all neccessary resources are loaded.
-		resourceLoaded = function () {
-
-			curLoadResNum += 1;
-			if (curLoadResNum >= totalLoadResources) {
-				redraw();
-			}
-		},
-
 		// Adds a point to the drawing array.
 		// @param x
 		// @param y
@@ -381,50 +296,10 @@ var drawingApp = (function () {
 			clickDrag.push(dragging);
 		},
 
-		// Creates a canvas element, loads images, adds events, and draws the canvas for the first time.
-		init = function () {
+		// Add mouse and touch event listeners to the canvas
+		createUserEvents = function () {
 
-			// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
-			var canvasDiv = document.getElementById('canvasDiv');
-			canvas = document.createElement('canvas');
-			canvas.setAttribute('width', canvasWidth);
-			canvas.setAttribute('height', canvasHeight);
-			canvas.setAttribute('id', 'canvas');
-			canvasDiv.appendChild(canvas);
-			if (typeof G_vmlCanvasManager !== "undefined") {
-				canvas = G_vmlCanvasManager.initElement(canvas);
-			}
-			context = canvas.getContext("2d"); // Grab the 2d canvas context
-			// Note: The above code is a workaround for IE 8 and lower. Otherwise we could have used:
-			//     context = document.getElementById('canvas').getContext("2d");
-
-			// Load images
-			crayonImage.onload = resourceLoaded;
-			crayonImage.src = "images/crayon-outline.png";
-
-			markerImage.onload = resourceLoaded;
-			markerImage.src = "images/marker-outline.png";
-
-			eraserImage.onload = resourceLoaded;
-			eraserImage.src = "images/eraser-outline.png";
-
-			crayonBackgroundImage.onload = resourceLoaded;
-			crayonBackgroundImage.src = "images/crayon-background.png";
-
-			markerBackgroundImage.onload = resourceLoaded;
-			markerBackgroundImage.src = "images/marker-background.png";
-
-			eraserBackgroundImage.onload = resourceLoaded;
-			eraserBackgroundImage.src = "images/eraser-background.png";
-
-			crayonTextureImage.onload = resourceLoaded;
-			crayonTextureImage.src = "images/crayon-texture.png";
-
-			outlineImage.onload = resourceLoaded;
-			outlineImage.src = "images/watermelon-duck-outline.png";
-
-			// Add mouse events
-			$('#canvas').mousedown(function (e) {
+			var press = function (e) {
 				// Mouse down location
 				var sizeHotspotStartX,
 					mouseX = e.pageX - this.offsetLeft,
@@ -472,23 +347,89 @@ var drawingApp = (function () {
 				paint = true;
 				addClick(mouseX, mouseY, false);
 				redraw();
-			});
+			},
 
-			$('#canvas').mousemove(function (e) {
-				if (paint) {
-					addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+				drag = function (e) {
+					if (paint) {
+						addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+						redraw();
+					}
+					// Prevent the whole page from dragging if on mobile
+					e.preventDefault();
+				},
+
+				release = function () {
+					paint = false;
 					redraw();
-				}
-			});
+				},
 
-			$('#canvas').mouseup(function () {
-				paint = false;
+				cancel = function () {
+					paint = false;
+				};
+
+			// Add mouse event listeners to canvas element
+			canvas.addEventListener("mousedown", press, false);
+			canvas.addEventListener("mousemove", drag, false);
+			canvas.addEventListener("mouseup", release);
+			canvas.addEventListener("mouseout", cancel, false);
+
+			// Add touch event listeners to canvas element
+			canvas.addEventListener("touchstart", press, false);
+			canvas.addEventListener("touchmove", drag, false);
+			canvas.addEventListener("touchend", release, false);
+			canvas.addEventListener("touchcancel", cancel, false);
+		},
+
+		// Calls the redraw function after all neccessary resources are loaded.
+		resourceLoaded = function () {
+
+			curLoadResNum += 1;
+			if (curLoadResNum === totalLoadResources) {
 				redraw();
-			});
+				createUserEvents();
+			}
+		},
 
-			$('#canvas').mouseleave(function () {
-				paint = false;
-			});
+		// Creates a canvas element, loads images, adds events, and draws the canvas for the first time.
+		init = function () {
+
+			// Create the canvas (Neccessary for IE because it doesn't know what a canvas element is)
+			canvas = document.createElement('canvas');
+			canvas.setAttribute('width', canvasWidth);
+			canvas.setAttribute('height', canvasHeight);
+			canvas.setAttribute('id', 'canvas');
+			document.getElementById('canvasDiv').appendChild(canvas);
+			if (typeof G_vmlCanvasManager !== "undefined") {
+				canvas = G_vmlCanvasManager.initElement(canvas);
+			}
+			context = canvas.getContext("2d"); // Grab the 2d canvas context
+			// Note: The above code is a workaround for IE 8 and lower. Otherwise we could have used:
+			//     context = document.getElementById('canvas').getContext("2d");
+
+			// Load images
+			crayonImage.onload = resourceLoaded;
+			crayonImage.src = "images/crayon-outline.png";
+
+			markerImage.onload = resourceLoaded;
+			markerImage.src = "images/marker-outline.png";
+
+			eraserImage.onload = resourceLoaded;
+			eraserImage.src = "images/eraser-outline.png";
+
+			crayonBackgroundImage.onload = resourceLoaded;
+			crayonBackgroundImage.src = "images/crayon-background.png";
+
+			markerBackgroundImage.onload = resourceLoaded;
+			markerBackgroundImage.src = "images/marker-background.png";
+
+			eraserBackgroundImage.onload = resourceLoaded;
+			eraserBackgroundImage.src = "images/eraser-background.png";
+
+			crayonTextureImage.onload = resourceLoaded;
+			crayonTextureImage.src = "images/crayon-texture.png";
+
+			outlineImage.onload = resourceLoaded;
+			outlineImage.src = "images/watermelon-duck-outline.png";
 		};
 
 	return {
